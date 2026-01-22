ed22a362ede1507251069501afe2651160236046
Ramakanth Kotha <ramakanth.kotha@innovapptive.com>
2026-01-23 02:31:45 +0530

    Add Vercel adapter for production rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  adapter: vercel()
 });
